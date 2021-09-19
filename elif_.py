b966a2d217dc2791abea727afbf4fb63f3de808d
python290821 <90146835+python290821@users.noreply.github.com>
2021-09-19 18:08:36 +0300

    Create elif_.py

---
+++ elif_.py
@@ -0,0 +1,13 @@
+#input a number. check if the number is 0 or 1, if so print ‘0 or 1’
+#otherwise check if the number is -1, if so print ‘-1’
+#otherwise print ‘unknown number’
+
+number = int(input('Please enter a number: '))
+if number == 0 or number == 1:
+    print('0 or 1')
+#else 
+# if number == -1:
+elif number == -1:
+  print('-1')
+else:
+  print('unknown number')
