ebf88ad7c12f91bc3b3647e41ffd07b2352c49a2
python290821 <90146835+python290821@users.noreply.github.com>
2021-09-19 17:56:39 +0300

    Add files via upload

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
 
 
-'''Targilim -- itayhau@gmail.com
+'''Targilim -- itayhau@gmail.com !
 1. input a number check if it is divided by 10 without reminder
      you need to check if it is % 5 and % 2 == 0, if so print ('can be divided by 10')
 '''
