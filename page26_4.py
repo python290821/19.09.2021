d0bf9cd52ba47eff7207eec3d10dd852f4d04171
python290821 <90146835+python290821@users.noreply.github.com>
2021-09-19 20:54:33 +0300

    Create page26_4.py

---
+++ page26_4.py
@@ -0,0 +1,11 @@
+# input a numbers
+max_ = int(input('Please enter a max number: '))
+den  = int(input('Please enter a den: '))
+
+i = den
+
+# iterate and print even numbers
+while i <= max_:
+    print(i)
+    i += den
+    
